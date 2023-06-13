12940aa0274c91b54bcc7274ce60e6ba5df228e5
Jona Tinoco <tinocojona@outlook.com>
2023-06-12 20:36:44 -0600

    lista de keywords respponsiva, tabla con iconos de accion, row seleccionable

--- assets/js/tabla.js
+++ assets/js/tabla.js
@@ -1,6 +1,57 @@
+var tableData = [
+    { id: 1, pagina: "Elecciones presidenciales", date: "23/02/2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 2, pagina: "La democracia en retroceso", date: "23/03/2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 3, pagina: "Elecciones presidenciales", date: "23/09/2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 4, pagina: "La democracia en retroceso", date: "23-05-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 5, pagina: "Elecciones presidenciales", date: "23-07-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 6, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 7, pagina: "Elecciones presidenciales", date: "23-10-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 8, pagina: "La democracia en retroceso", date: "23-11-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 1, pagina: "Elecciones presidenciales", date: "23-12-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 1, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 1, pagina: "Elecciones presidenciales", date: "23-09-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 1, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 1, pagina: "Elecciones presidenciales", date: "23-09-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 1, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 1, pagina: "Elecciones presidenciales", date: "23-09-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 1, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 1, pagina: "Elecciones presidenciales", date: "23-09-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 1, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 1, pagina: "Elecciones presidenciales", date: "23-09-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 1, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 1, pagina: "Elecciones presidenciales", date: "23-09-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 1, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 1, pagina: "Elecciones presidenciales", date: "23-09-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 1, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 1, pagina: "Elecciones presidenciales", date: "23-09-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 1, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 1, pagina: "Elecciones presidenciales", date: "23-09-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 1, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" },
+    { id: 1, pagina: "Elecciones presidenciales", date: "23-09-2001", estado: "Veracruz", url: "www.gob.mx/elecciones", zona: "Federal", categoria: "Elecciones", acciones: "Acciones" },
+    { id: 1, pagina: "La democracia en retroceso", date: "23-09-2001", estado: "Cuernavaca", url: "www.gob.mx/democracia", zona: "Federal", categoria: "Democracia", acciones: "Acciones" }
+]
+
+
+var printIcon = function (cell, formatterParams) { //plain text value
+    return "<i data-bs-toggle='modal' data-bs-target='#exampleModal' class='bi bi-eye fs-5 me-1'></i> <i class='bi bi-trash fs-5'></i>";
+};
+
 var table = new Tabulator("#example-table", {
-    height:550,
-    placeholder: "No hay datos para mostrar",
-    layout:"fitColumns",
-    resizableColumnFit:true
+    layout:"fitDataStretch",
+    data: tableData, //set initial table data
+    pagination:true,
+    columns: [
+        {
+            formatter: "rowSelection", titleFormatter: "rowSelection", hozAlign: "center", headerSort: false, cellClick: function (e, cell) {
+                cell.getRow().toggleSelect();
+            }
+        },
+        { title: "Página", field: "pagina" },
+        { title: "Fecha de publicación", field: "date", sorter: "date" },
+        { title: "Estado", field: "estado" },
+        { title: "URL", field: "url" },
+        { title: "Zona", field: "zona" },
+        { title: "Categoría", field: "categoria" },
+        { title: "Acciones", formatter: printIcon, width: 107, hozAlign: "center"},
+    ],
 });
