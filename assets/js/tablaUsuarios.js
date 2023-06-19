6caf722fcbc736598ac0c1fd438ee4a8293f0b9b
Jona Tinoco <tinocojona@outlook.com>
2023-06-19 14:21:01 -0600

    usuarios, cambio de tamaño titulo de cada pagina

---
+++ assets/js/tablaUsuarios.js
@@ -0,0 +1,55 @@
+var tableData = [
+    { id: 1, nombre:"Aldo Mujica Santana", date:"19/06/2023"},
+    { id: 2, nombre:"Jonathan Mujica Santana", date:"19/06/2023"},
+    { id: 3, nombre:"Mario Mujica Santana", date:"19/06/2023"},
+    { id: 4, nombre:"Anahi Mujica Santana", date:"19/06/2023"},
+    { id: 5, nombre:"Maria Mujica Santana", date:"19/06/2023"},
+    { id: 6, nombre:"Aldo Mujica Santana", date:"19/06/2023"},
+    { id: 7, nombre:"Jonathan Mujica Santana", date:"19/06/2023"},
+    { id: 8, nombre:"Mario Mujica Santana", date:"19/06/2023"},
+    { id: 9, nombre:"Anahi Mujica Santana", date:"19/06/2023"},
+    { id: 10, nombre:"Marco Mujica Santana", date:"19/06/2023"},
+    { id: 11, nombre:"Alan Mujica Santana", date:"19/06/2023"},
+    { id: 12, nombre:"Paola Mujica Santana", date:"19/06/2023"},
+    { id: 13, nombre:"Alin Mujica Santana", date:"19/06/2023"},
+    { id: 14, nombre:"Abril Mujica Santana", date:"19/06/2023"},
+    { id: 15, nombre:"Joselu Mujica Santana", date:"19/06/2023"},
+    { id: 16, nombre:"Jose Mujica Santana", date:"19/06/2023"},
+    { id: 17, nombre:"Edgar Mujica Santana", date:"19/06/2023"},
+    { id: 18, nombre:"Tomas Mujica Santana", date:"19/06/2023"},
+    { id: 19, nombre:"Pedro Mujica Santana", date:"19/06/2023"},
+    { id: 20, nombre:"Juan Mujica Santana", date:"19/06/2023"},
+]
+
+
+var printIcon = function (cell, formatterParams) { //plain text value
+    return "<i class='bi bi-pen fs-5 me-1'></i> <i class='bi bi-trash fs-5'></i>";
+};
+
+var table = new Tabulator("#example-table", {
+    layout: "fitDataStretch",
+    data: tableData, //set initial table data
+    pagination: "true",
+    pagination: true,
+    paginationSize: 10,
+    paginationSizeSelector: [10, 25, 50, 100, true],
+    paginationCounter: "rows",
+    columns: [
+        {
+            formatter: "rowSelection", titleFormatter: "rowSelection", hozAlign: "center", headerHozAlign: "center", resizable: false, headerSort: false, cellClick: function (e, cell) {
+                cell.getRow().toggleSelect();
+            }
+        },
+        { title: "ID", field: "id", sorter:"number" },
+        { title: "Nombre", field: "nombre"},
+        { title: "Fecha de registro", field: "date", sorter: "date" },
+        { title: "Acciones", formatter: printIcon, width: 107, hozAlign: "center", headerHozAlign: "center", headerSort: false },
+    ],
+});
+
+table.on("rowSelected", function (row) {
+    //row - row component for the selected row
+
+    var rowData = row.getData()
+    console.log(rowData);
+});
